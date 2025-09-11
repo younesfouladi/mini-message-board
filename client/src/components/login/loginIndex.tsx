8e22e74a012b2c69ef1f57a561a4dce55edea28c
Younes <mehrabtahami@gmail.com>
2025-09-11 17:44:06 +0330

    added a pretty custom form validity for user name input

--- client/src/components/login/loginIndex.tsx
+++ client/src/components/login/loginIndex.tsx
@@ -1,6 +1,6 @@
 import type React from "react";
 import { useUserLogin } from "../../hooks/useUserLogin";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Tooltip } from "@heroui/tooltip";
 
 export default function Login() {
@@ -8,6 +8,15 @@ export default function Login() {
   const setUserId = useUserLogin((states) => states.setUserId);
   const [isValid, setIsValid] = useState(false);
   const [error, setError] = useState("");
+  const userNameRef = useRef<HTMLInputElement | null>(null);
+
+  const handleValidity = () => {
+    if (!userNameRef.current?.checkValidity()) {
+      setError("!REQUIRED - at least 5 characters");
+    } else {
+      setError("");
+    }
+  };
 
   const handleLogin = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
@@ -22,7 +31,7 @@ export default function Login() {
         <div className="bg-[url('./images/login-bg.webp')] w-[clamp(200px,70vw,500px)] aspect-square bg-cover rounded-2xl"></div>
         <div className="w-full px-4">
           <form className="flex flex-col items-center gap-6 my-8">
-            <label className="flex gap-2 items-center">
+            <label className="flex gap-2 items-center relative">
               <input
                 type="text"
                 name="name"
@@ -30,29 +39,33 @@ export default function Login() {
                 className="px-4 py-2 w-full bg-main rounded-4xl user-invalid:border-red-700 user-invalid:border-2 user-valid:border-green-700 user-valid:border-2"
                 minLength={5}
                 required
+                ref={userNameRef}
+                onChange={handleValidity}
               />
               {error !== "" ? (
-                <Tooltip
-                  key="formerror"
-                  className=""
-                  color="danger"
-                  content={error}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="size-6 text-red-600"
+                <div className="absolute right-2">
+                  <Tooltip
+                    key="formerror"
+                    className=""
+                    color="danger"
+                    content={error}
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
-                    />
-                  </svg>
-                </Tooltip>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-7 text-red-600"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+                      />
+                    </svg>
+                  </Tooltip>
+                </div>
               ) : null}
             </label>
             <button
